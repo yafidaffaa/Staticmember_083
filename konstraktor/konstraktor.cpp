d7ea0d708b8adbe84a649e599259da06609dda61
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-05-17 19:36:35 +0700

    method cetak() dalam class mahasiswa untuk mencetak nim dan nama

--- konstraktor/konstraktor.cpp
+++ konstraktor/konstraktor.cpp
@@ -31,3 +31,8 @@ mahasiswa::mahasiswa(int iNim, string iNama) {
 	nim = iNim;
 	nama = iNama;
 }
+
+void mahasiswa::cetak() {
+	cout << endl << "Nim = " << nim << endl;
+	cout << " Nama = " << nama << endl;
+}
