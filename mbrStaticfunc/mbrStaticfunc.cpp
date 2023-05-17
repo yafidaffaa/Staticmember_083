979441155a66ffba76bc9837d9feed116690e30f
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-05-17 19:23:26 +0700

    memberikan nilai string di mhs 1,2,3,4 dan memberikan nim 9 + 1 di mhs 3 dan 10 + 1 di mhs 4. kemudian menampilkan semuanya

--- mbrStaticfunc/mbrStaticfunc.cpp
+++ mbrStaticfunc/mbrStaticfunc.cpp
@@ -29,3 +29,20 @@ void mahasiswa::printAll() {
 	cout << "Nama = " << nama << endl;
 	cout << endl;
 }
+
+int main() {
+	mahasiswa mhs1("Sri Dadi");
+	mahasiswa mhs2("Budi Jatmiko");
+	mahasiswa::setNim(9); //mengkases nim melalui static member function "setNim"
+	mahasiswa mhs3("Andi Janu");
+	mahasiswa mhs4("Joko Wahono");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+	cout << "akses dari luar object = " << mahasiswa::getNim() << endl; //mengkases nim melalui static member function "getNim"
+
+	return 0;
+}
