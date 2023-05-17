e5c25b0fa523f7792bb3c833bb9d3a0d00b7581f
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-05-17 19:17:12 +0700

    void untuk menampilkan id dan nama

--- mbrStaticfunc/mbrStaticfunc.cpp
+++ mbrStaticfunc/mbrStaticfunc.cpp
@@ -23,3 +23,9 @@ int mahasiswa::nim = 0;
 void mahasiswa::setID() {
 	id = ++nim;
 }
+
+void mahasiswa::printAll() {
+	cout << "ID   = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
