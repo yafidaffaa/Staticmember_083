d003d3611ca61725279f65a16308f79575c8b63c
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-05-17 10:55:08 +0700

    membuat class mahasiswa dengan public akses modifire dengan 5 member

--- mbrStatic/mbrStatic.cpp
+++ mbrStatic/mbrStatic.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
