418ed5b016b27526931f9b7e0d082806ad0dff09
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-05-17 11:02:02 +0700

    membuat method mahasiswa dilingkup printAll() yang berguna untuk print id dan nama

--- mbrStatic/mbrStatic.cpp
+++ mbrStatic/mbrStatic.cpp
@@ -18,3 +18,9 @@ int mahasiswa::nim = 0;
 void mahasiswa::setID() {
 	id = ++nim;
 }
+
+void mahasiswa::printAll() {
+	cout << "ID   = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
