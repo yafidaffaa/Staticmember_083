fbca570397e3bcc3bd3df27b8210f03e8c962cc5
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-05-17 11:12:01 +0700

    menambahkan mhs2.nim = 10 setelah asroni dan menambahkan mhs3.nim = 20 setelah andi kurniawan

--- mbrStatic/mbrStatic.cpp
+++ mbrStatic/mbrStatic.cpp
@@ -28,7 +28,9 @@ void mahasiswa::printAll() {
 int main() {
 	mahasiswa mhs1("Lia Kurnia");
 	mahasiswa mhs2("Asroni");
+	mhs2.nim = 10;
 	mahasiswa mhs3("Andi Kurniawan");
+	mhs3.nim = 20;
 	mahasiswa mhs4("Joko Purbo");
 
 	mhs1.printAll();
