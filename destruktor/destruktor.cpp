1559c9537b7866d73193f4dd4fd03080c55fe853
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-05-17 20:13:32 +0700

    membuat object belajarcpp dari class angka dan memberikan argumen 3, membuat pointer ptrBelajarcpp dan menggunakan operator new untuk membuat object angka dengan argumen 5, menghapus object yang dialokasikan ptrBelajarcpp dengan operator delete

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -39,3 +39,11 @@ void angka::isiData() {
 	cout << endl;
 }
 
+int main() {
+	angka belajarcpp(3); //comstructor dipanggil
+	angka* ptrBelajarcpp = new angka(5); //constructor dipanggil
+	delete ptrBelajarcpp; //destructor dipanggil
+
+	return 0;
+}//destructor dipanggil
+
